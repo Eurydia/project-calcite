52d6ebf2e738eb7ade65c72a9f4489b3c4f88125
Eurydia <crossdeltavi@gmail.com>
2025-03-09 14:02:28 +0100

    added favicon and project name

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ThemeProvider } from "@emotion/react";
+import { Editor } from "@monaco-editor/react";
+import {
+  BorderColorRounded,
+  FormatQuoteRounded,
+} from "@mui/icons-material";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
+import {
+  Box,
+  createTheme,
+  CssBaseline,
+  responsiveFontSizes,
+  Tab,
+  Toolbar,
+} from "@mui/material";
+import { FC, useEffect, useState } from "react";
+import { unified } from "unified";
+let theme = createTheme({
+  typography: { htmlFontSize: 20 },
+});
+theme = responsiveFontSizes(theme);
 
-function App() {
-  const [count, setCount] = useState(0)
+import remarkGfm from "remark-gfm";
+import remarkParse from "remark-parse";
+import remarkStringify from "remark-stringify";
 
+const parser = unified()
+  .use(remarkParse)
+  .use(remarkGfm)
+  .use(remarkStringify);
+
+export const App: FC = () => {
+  const [tab, setTab] = useState<0 | 1>(0);
+  const [content, setContent] = useState<
+    string | undefined
+  >(undefined);
+  useEffect(() => {
+    console.debug(String(parser.processSync(content)));
+  }, [content]);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box>
+        <TabContext value={tab}>
+          <Toolbar>
+            <TabList onChange={(_, value) => setTab(value)}>
+              <Tab
+                icon={<BorderColorRounded />}
+                iconPosition="start"
+                label="Editor"
+                value={0}
+              />
+              <Tab
+                icon={<FormatQuoteRounded />}
+                iconPosition="start"
+                label="Result"
+                value={1}
+              />
+            </TabList>
+          </Toolbar>
+          <TabPanel
+            value={0}
+            sx={{
+              height: "70vh",
+            }}
+          >
+            <Editor
+              value={content}
+              onChange={(value) => setContent(value)}
+              height="100%"
+              language="markdown"
+              options={{
+                fontSize: theme.typography.htmlFontSize,
+                wordWrap: "bounded",
+                scrollBeyondLastLine: false,
+                minimap: { enabled: false },
+              }}
+            />
+          </TabPanel>
+          <TabPanel value={1}></TabPanel>
+        </TabContext>
+      </Box>
+    </ThemeProvider>
+  );
+};
 
-export default App
